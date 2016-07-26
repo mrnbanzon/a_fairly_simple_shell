ad0b7afc36b0657d4fc04a0e6d5676b0d6127156
Michael <mrnbanzon@gmail.com>
2016-07-26 09:42:42 -0700

    command class

--- commands.h
+++ commands.h
@@ -3,6 +3,18 @@
 #include <stdlib.h>
 #include "unistd.h"
 
+//command option class
+class option {
+	std::string opt;
+	option* next;
+};
+
+//commands class
+class command {
+	std::string command;
+	
+};
+
 void pwdFunc();
 void histFunc(); 
 void cdFunc(); 
