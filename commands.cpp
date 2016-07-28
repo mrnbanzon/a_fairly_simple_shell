7d856a83c31bef9d1c0869d8b7415e347661be75
Michael Banzon <mrnbanzon@gmail.com>
2016-07-27 20:07:31 -0700

    tab alignment

--- commands.cpp
+++ commands.cpp
@@ -5,7 +5,7 @@
 void pwdFunc() {	
 	char newline = '\n';
 	char *path = getenv("PWD");
-  write(STDOUT_FILENO, path, strlen(path));
+	write(STDOUT_FILENO, path, strlen(path));
 	write(STDOUT_FILENO, &newline, 1);
 }
 
